d5c5e46acb440fda876eb4c905967d1f2598a169
jessicaycc <jessicaycc.chen@mail.utoronto.ca>
2020-02-23 17:54:51 -0500

    A2 template

---
+++ a2_bleu_score.py
@@ -0,0 +1,93 @@
+# Copyright 2020 University of Toronto, all rights reserved
+
+'''Calculate BLEU score for one reference and one hypothesis
+
+You do not need to import anything more than what is here
+'''
+
+from math import exp  # exp(x) gives e^x
+
+
+def grouper(seq, n):
+    '''Extract all n-grams from a sequence
+
+    An n-gram is a contiguous sub-sequence within `seq` of length `n`. This
+    function extracts them (in order) from `seq`.
+
+    Parameters
+    ----------
+    seq : sequence
+        A sequence of words or token ids representing a transcription.
+    n : int
+        The size of sub-sequence to extract.
+
+    Returns
+    -------
+    ngrams : list
+    '''
+    assert False, "Fill me"
+
+
+def n_gram_precision(reference, candidate, n):
+    '''Calculate the precision for a given order of n-gram
+
+    Parameters
+    ----------
+    reference : sequence
+        The reference transcription. A sequence of words or token ids.
+    candidate : sequence
+        The candidate transcription. A sequence of words or token ids
+        (whichever is used by `reference`)
+    n : int
+        The order of n-gram precision to calculate
+
+    Returns
+    -------
+    p_n : float
+        The n-gram precision. In the case that the candidate has length 0,
+        `p_n` is 0.
+    '''
+    assert False, "Fill me"
+
+
+def brevity_penalty(reference, candidate):
+    '''Calculate the brevity penalty between a reference and candidate
+
+    Parameters
+    ----------
+    reference : sequence
+        The reference transcription. A sequence of words or token ids.
+    candidate : sequence
+        The candidate transcription. A sequence of words or token ids
+        (whichever is used by `reference`)
+
+    Returns
+    -------
+    BP : float
+        The brevity penalty. In the case that the candidate transcription is
+        of 0 length, `BP` is 0.
+    '''
+    assert False, "Fill me"
+
+
+def BLEU_score(reference, hypothesis, n):
+    '''Calculate the BLEU score
+
+    Parameters
+    ----------
+    reference : sequence
+        The reference transcription. A sequence of words or token ids.
+    candidate : sequence
+        The candidate transcription. A sequence of words or token ids
+        (whichever is used by `reference`)
+    n : int
+        The maximum order of n-gram precision to use in the calculations,
+        inclusive. For example, ``n = 2`` implies both unigram and bigram
+        precision will be accounted for, but not trigram.
+
+    Returns
+    -------
+    bleu : float
+        The BLEU score
+    '''
+    assert False, "Fill me"
